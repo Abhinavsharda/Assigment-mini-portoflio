75f03070376ed53d67c7f44042137408902be6de
asharda1 <300868625@cencol.ca>
2016-06-03 21:04:09 -0400

    new commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,7 +7,8 @@
 * StudentID: 300868625
 * website: Assigment 1
 * @description This contain javascript 
-*/
+*/ 
+
 var content = [
     "Abhinav Sharda",
     "I was born in India on may 07,1997. I was born and raised in middle class family.our family has great affection with animals so we are always ready help any needy animals. I persuade all my studies in CBSE .having agreat intrest in computers made me to decide to came canada for my higher qualification in the technology field. I hope to get a good job in this field."
