4cab066e70bd9f1165c8eb4d2cc2039b03b62f36
asharda1 <300868625@cencol.ca>
2016-06-03 21:14:49 -0400

    Projects commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,11 +11,26 @@
 
 var content = [
     "Abhinav Sharda",
-    "I was born in India on may 07,1997. I was born and raised in middle class family.our family has great affection with animals so we are always ready help any needy animals. I persuade all my studies in CBSE .having agreat intrest in computers made me to decide to came canada for my higher qualification in the technology field. I hope to get a good job in this field."
-];
+    "I was born in India on may 07,1997. I was born and raised in middle class family.our family has great affection with animals so we are always ready help any needy animals. I persuade all my studies in CBSE .having agreat intrest in computers made me to decide to came canada for my higher qualification in the technology field. I hope to get a good job in this field.",
+    "Projects",
+     "First project - composition of lead battery",
+    "",
+    "Second Project - Composition of gujarat sand",
+    "",
+    "Third Project - Finiacing the needy people ",
+    ""
+    ];
  
 (function(){
-    document.getElementById('Name').textContent = protfolio[0];
-    document.getElementById('Discription').textContent = protfolio[1];
+   var keys = []
+    keys[0] = document.getElementById('Name');
+    keys[1] = document.getElementById('Portfolio');
+    keys[2] = document.getElementById('projects');
+    keys[3] = document.getElementById('projects1');
+    keys[4] = document.getElementById('about1');
+    keys[5] = document.getElementById('projects2');
+    keys[6] = document.getElementById('about2');
+    keys[7] = document.getElementById('projects3');
+    keys[8] = document.getElementById('about3');
     
 })();
